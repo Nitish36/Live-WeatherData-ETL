106ba53f4a2983c17f847cecf1e0070b06f4d1d1
Nitish k <1rn19cs092.nitish.pkv@gmail.com>
2024-12-14 16:14:02 +0530

    Delete fastAPIS.py

--- fastAPIS.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI,Path
-import json
-app = FastAPI()
-
-@app.get("/get")
-def home():
-    with open('LiveWeatherData.json', 'r') as file:
-        jsoneddata = json.load(file)
-        return jsoneddata
