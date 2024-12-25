8642392d05a21e9f42a86d2fe37bd0bfd7ade928
Nitish k <1rn19cs092.nitish.pkv@gmail.com>
2024-12-25 11:54:59 +0530

    Update weather_api.py

--- weather_api.py
+++ weather_api.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI,Path
 import json
 app = FastAPI()
 
-@app.get("/get")
+@app.get("/")
 def home():
     with open('datasets/WeatherData.json', 'r') as file:
         jsoneddata = json.load(file)
